47420eb1b2b2ebb27dcbfb139ce31c5d87da7d83
Osric Wilkinson <gitmoose@fluffypeople.com>
2017-12-31 00:06:05 +0000

    Working, but needs to be faster

--- src/main/java/com/moosemorals/videoserver/VideoHandler.java
+++ src/main/java/com/moosemorals/videoserver/VideoHandler.java
@@ -8,21 +8,80 @@ import org.apache.http.protocol.HttpRequestHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class VideoHandler implements HttpRequestHandler {
+public class VideoHandler implements HttpRequestHandler, Runnable {
 
     private final Logger log = LoggerFactory.getLogger(VideoHandler.class);
+
+    private final Object lock = new Object();
+    private int state = 0;
 
     @Override
     public void handle(HttpRequest httpRequest, HttpResponse response, HttpContext httpContext) throws HttpException, IOException {
         log.debug("Handling request");
 
-        VideoEntity body = new VideoEntity();
+        new Thread(this, "Transcoder").start();
 
-        response.setStatusCode(200);
-        response.setEntity(body);
+        // Wait for the process to start
+        synchronized (lock) {
+            while (state == 0) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    throw new IOException("Unexpected interruption", e);
+                }
+            }
+        }
 
+        response.setStatusCode(302);
+        response.setHeader("Location", "http://localhost:8081");
     }
 
+
+    @Override
+    public void run() {
+        try {
+            log.debug("Starting process");
+            Process ffmpeg = new ProcessBuilder(
+                    "C:\\Users\\Osric\\Desktop\\ffmpeg-20171229-0c78b6a-win64-static\\bin\\ffmpeg.exe",
+                    "-i", "C:\\Users\\Osric\\Desktop\\tg.mpg",
+                    "-c:v", "libvpx-vp9",
+                    "-crf", "31",
+                    "-b:v", "0",
+                    "-deadline", "realtime",
+                    "-listen", "1",
+                    "-f", "webm",
+                    "http://localhost:8081"
+            ).start();
+
+            new Thread(() -> {
+                try (BufferedReader err = new BufferedReader(new InputStreamReader(ffmpeg.getErrorStream()))) {
+
+                    String line;
+                    while ((line = err.readLine()) != null) {
+                        log.error("FFMPEG: {}", line);
+                    }
+                } catch (IOException ex) {
+                    log.error("Problem reading error stream", ex);
+                }
+            }, "Errors").start();
+
+            try {
+                synchronized (lock) {
+                    state = 1;
+                    lock.notifyAll();
+                }
+                ffmpeg.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
